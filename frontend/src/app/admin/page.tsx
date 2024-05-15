2ec983bdff0c9f0e29466284bfa1449b20d374f2
yc1121 <wherechi0912@gmail.com>
2024-05-15 16:01:32 +0800

    feat: official speech list in admin page

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -21,6 +21,11 @@ export default function Page() {
           辦理官方講座
         </Link>
       </Button>
+      <Button className="bg-white text-black rounded-lg h-[10vh] hover:bg-neutral-100 transition font-semibold">
+        <Link href="/user/profile" className="text-sm lg:text-lg">
+          官方講座列表
+        </Link>
+      </Button>
     </main>
   );
 }
