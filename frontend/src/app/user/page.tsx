cf43f87f565b5bc16ba7558d4e744827c079234d
ChunHao-Yang <ginocollege@gmail.com>
2024-05-26 17:05:40 +0800

    add navbar for user and admin2

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -14,22 +14,5 @@ export default function Page() {
 
   return (
     <></>
-    // <main className="flex flex-col justify-center space-y-6">
-    //   <Button className={`bg-white text-black rounded-lg h-[10vh] hover:bg-neutral-100 transition font-semibold`}>
-    //     <Link href="/user/profile" className="text-sm lg:text-lg">
-    //       Profile
-    //     </Link>
-    //   </Button>
-    //   <Button className="bg-white text-black rounded-lg h-[10vh] hover:bg-neutral-100 transition font-semibold">
-    //     <Link href="/user/profile" className="text-sm lg:text-lg">
-    //       Activity List
-    //     </Link>
-    //   </Button>
-    //   <Button className="bg-white text-black rounded-lg h-[10vh] hover:bg-neutral-100 transition font-semibold">
-    //     <Link href="/user/profile" className="text-sm lg:text-lg">
-    //       Create Activities
-    //     </Link>
-    //   </Button>
-    // </main>
   );
 }
