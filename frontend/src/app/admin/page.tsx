a1d4285ce24e8ea7cdc2f882ec9ff4a3d5cffaa2
LaurenceShen <laurence220016@gmail.com>
2024-05-29 16:40:37 +0800

    remove redirect in admin page

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -2,16 +2,30 @@
 
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-import { useEffect } from 'react';
-import { usePathname } from 'next/navigation';
 
 export default function Page() {
-  const path = usePathname();
-
-  useEffect(() => {
-    window.location.href = '/admin/activity';
-  }, []);
   return (
-    <></>
+    <main className="flex flex-col justify-center space-y-6">
+      <Button className="bg-white text-black rounded-lg h-[10vh] hover:bg-neutral-100 transition font-semibold">
+        <Link href="/admin/profile" className="text-sm lg:text-lg">
+          監控用戶流量
+        </Link>
+      </Button>
+      <Button className="bg-white text-black rounded-lg h-[10vh] hover:bg-neutral-100 transition font-semibold">
+        <Link href="/admin/audit_activity" className="text-sm lg:text-lg">
+          管理活動
+        </Link>
+      </Button>
+      <Button className="bg-white text-black rounded-lg h-[10vh] hover:bg-neutral-100 transition font-semibold">
+        <Link href="/admin/create_activities" className="text-sm lg:text-lg">
+          辦理官方講座
+        </Link>
+      </Button>
+      <Button className="bg-white text-black rounded-lg h-[10vh] hover:bg-neutral-100 transition font-semibold">
+        <Link href="/user/profile" className="text-sm lg:text-lg">
+          官方講座列表
+        </Link>
+      </Button>
+    </main>
   );
 }
