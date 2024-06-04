ba28173d58f8e72e63dbd67409b1483dc86c511c
LaurenceShen <laurence220016@gmail.com>
2024-06-04 20:48:13 +0800

    delete useless lines

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,7 +2,6 @@
 
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { format } from 'react-string-format';
 
 import type { ActivityData } from '@/lib/shared_types';
 import { FaLocationDot } from 'react-icons/fa6';
